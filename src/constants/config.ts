687498f7245d66bcc904f8fdb728d4342721cae8
sugarycookies <sugarcookieeater@gmail.com>
2024-12-13 18:46:06 -0500

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,8 +1,8 @@
 export const CONFIG = {
-  tries: 7, // This changes how many tries you get to finish the game
-  language: 'YourLanguageHere', // This changes the display name for your language
+  tries: 6, // This changes how many tries you get to finish the game
+  language: 'EORATASU', // This changes the display name for your language
   wordLength: 6, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this game!
+  author: 'VI', // Put your name here so people know who made this game!
   authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
   wordListSource: 'YourDictionarySource', // Describe the source material for your words here
   wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
