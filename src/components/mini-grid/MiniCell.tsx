d7a9b5b2158ec88c215d940c9d19315c409f4ccd
sugarycookies <sugarcookieeater@gmail.com>
2024-12-14 02:07:20 +0000

    some other change idrk

--- src/components/mini-grid/MiniCell.tsx
+++ src/components/mini-grid/MiniCell.tsx
@@ -11,8 +11,8 @@ export const MiniCell = ({ status, letter }: Props) => {
     'w-10 h-10 border-solid border-2 border-slate-200 flex items-center justify-center mx-0.5 text-lg font-bold rounded',
     {
       'bg-white': status === 'absent',
-      'bg-purple-500': status === 'correct',
-      'bg-orange-500': status === 'present',
+      'bg-green-500': status === 'correct',
+      'bg-yellow-500': status === 'present',
     }
   )
 
